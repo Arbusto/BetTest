a409d02ceba46fbb2e86f277750e370b28257b1d
Augusto Schiavi <A.Schiavi@gmail.com>
2016-08-01 16:03:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/', function(req, res) {
 	res.sendFile('public/views/index.html', { root : __dirname}); // load the single view file (angular will handle the page changes on the front-end)
 });
 
-app.listen(3000);
+app.listen(80);
